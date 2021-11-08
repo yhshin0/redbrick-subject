509b58a58eb250f3404899d1509859074de527c8
BangleCoding <effectivecodeyr@gmail.com>
2021-11-08 23:04:18 +0900

    createUser 기능 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() createBody: CreateUserDto) {
+    const result = await this.usersService.createUser(createBody);
+    if (result.ok) {
+      return {
+        message: '회원가입에 성공하였습니다.',
+      };
+    } else {
+      throw new HttpException(result.error, result.htmlStatus);
+    }
+  }
+}
