fa956e4ad84c205192774721f10ed8f908479599
doogler777 <doogler777@gmail.com>
2021-11-08 21:59:56 +0900

    fix:users 모듈에 컨트롤러, 서비스 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,7 +18,6 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
     UsersModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
