b7019f24075d920e0dc9cb909a5b7724a88bac44
Gibson Lee <gb.lee.gibson@gmail.com>
2021-11-08 18:24:31 +0900

    chore: 초기 설정
    - TypeORM 설치 및 설정
    - class validator & class transformer 설치 및 설정
    - ConfigModule 설치 및 설정
    - 유저 인증 관련 패키지 설치
    resolves #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'redbrick.db',
+      entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
