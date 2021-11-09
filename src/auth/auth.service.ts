cb0b1a064edb50f10fc6c1c93e54a10e191fa6c7
BangleCoding <effectivecodeyr@gmail.com>
2021-11-09 15:57:48 +0900

    fix : service 경로 변경

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from '../users/users.service';
+import { UsersService } from 'src/users/users.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import * as bcrypt from 'bcrypt';
 
