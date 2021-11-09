b7c377fef8bd691bfb5e15a6548d50b605492cb2
doogler777 <doogler777@gmail.com>
2021-11-09 14:43:19 +0900

    fix:users생성시 암호화 부분 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -24,6 +24,7 @@ export class User extends CoreEntity {
   @Column({ type: 'datetime', nullable: true })
   loginedAt: Date;
 
+  //유저 생성시 암호화부분
   @BeforeInsert()
   async hashPassword(): Promise<void> {
     try {
