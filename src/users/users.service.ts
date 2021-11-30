b2a6179d19ed307a586d2c7df65b5ca52a7f2e6a
yhshin0 <asyh1047@naver.com>
2021-11-30 20:47:07 +0900

    fix: user update
    - user 업데이트 시 password, nickname을 모두 입력하지 않아도 가능하도록 수정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,11 +5,11 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-import * as bcrypt from 'bcrypt';
 import { USER_CONSTANTS, USER_ERROR_MSG } from './user.constants';
 
 @Injectable()
@@ -24,7 +24,7 @@ export class UsersService {
     password,
     nickname,
   }: CreateUserDto): Promise<User> {
-    const existUser = await this.usersRepository.findOne({ email });
+    const existUser = await this.findOne(email);
     if (existUser) {
       throw new ConflictException(USER_ERROR_MSG.EXISTED_EMAIL);
     }
@@ -45,20 +45,20 @@ export class UsersService {
     return await this.usersRepository.findOne({ email });
   }
 
-  // 로그인한 유저 시각 갱신
+  // 로그인 시각 갱신
   async updateLoginedAt(email: string, loginedAt: Date): Promise<void> {
     const user = await this.findOne(email);
-    delete user.password;
     user.loginedAt = loginedAt;
     await this.usersRepository.save(user);
   }
 
   async updateUser(email: string, updateUserDto: UpdateUserDto): Promise<User> {
-    const user: User = await this.usersRepository.findOne({ email });
+    const user = await this.findOne(email);
     const { nickname, password } = updateUserDto;
-
-    user.nickname = nickname;
-    user.password = await bcrypt.hash(password, USER_CONSTANTS.SALT_ROUND);
+    user.nickname = nickname || user.nickname;
+    user.password = password
+      ? await bcrypt.hash(password, USER_CONSTANTS.SALT_ROUND)
+      : user.password;
 
     try {
       return await this.usersRepository.save(user);
